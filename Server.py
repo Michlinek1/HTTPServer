ff8d9a5310eeed715d840a5c2438f847327c3079
Michlinek1 <94484725+Michlinek1@users.noreply.github.com>
2022-03-29 16:17:18 +0200

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,4 +1,5 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
+import glob
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -14,18 +15,32 @@ class handler(BaseHTTPRequestHandler):
         
 def main():
     global plik
+    htmllista = [f for f in glob.glob("*.html")] #sprawdza plikiw danym folderze o rozszerzeniu .html
     portinput = int(input("Podaj port:"))
-    if portinput == 0 or portinput == '':
+    if portinput == 0:
         print("Spróbuj ponownie!")
         main()
-    NazwaPliku = input("Podaj nazwę pliku html:")
-    if NazwaPliku == "":
-        print("Nazwa pliku nie może być pusta!")
-        main()
+    while True:
+        print(f"Lista plików: {' '.join(htmllista)}") # .join konwertuje listę do stringa
+        NazwaPliku = input("Podaj nazwę pliku html:")
+        if NazwaPliku == "":
+            print("Nazwa pliku nie może być pusta!")
+        elif ".html" not in NazwaPliku:
+            print("Nazwa pliku nie ma końcowki .html!")
+        elif NazwaPliku not in htmllista:
+            pytanie = str(input("Nie istnieje taki plik! Chcesz go stworzyć?"))
+            if pytanie == "tak" or pytanie == "Tak":
+                f = open(NazwaPliku, "w")
+                f.close()
+                print("Plik został stworzony!")
+                break
+        else:
+            break
+            
     port = portinput #port
     plik = NazwaPliku
-    serwer = HTTPServer(('', port), handler)
-    print(f"Serwer działa na porcie {port}, nazwa pliku: {plik}")
+    serwer = HTTPServer(('', portinput), handler)
+    print(f"Serwer działa na porcie {port}, nazwa pliku: {NazwaPliku}")
     serwer.serve_forever()
     
 
